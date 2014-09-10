ade0f9bd1646306069812ec433cea3e2ec216cf0
Kiril Zvezdarov <kzvezdarov@gmail.com>
2014-09-10 14:48:55 -0400

    Added setup.py script

---
+++ setup.py
@@ -0,0 +1,10 @@
+#! /usr/bin/env python2.7
+
+setup(name="ircdd",
+      version="0.1",
+      description="Distributed IRC Daemon",
+      url="github.com/kzvezdarov/ircdd",
+      license="GPL v3.0 or later",
+      packages=["ircdd"],
+      scripts=["bin/ircdd.py"],
+      )
