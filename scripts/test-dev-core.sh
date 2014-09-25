39ea523e0141e32fba3a0954ebaee96f9200810d
Kiril Zvezdarov <kzvezdarov@gmail.com>
2014-09-25 13:39:10 -0400

    Set up test scripts and fixed Docekrfile/service for dev machine

---
+++ scripts/test-dev-core.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/bash
+
+cd `dirname $0`/dev-config
+vagrant rsync
+vagrant ssh -c "fleetctl destroy ircdd && fleetctl start --block-attempts=100 ircdd/scripts/dev-config/services/ircdd.service"
+
+if vagrant ssh -c "fleetctl status ircdd" | grep "failed"; then
+    vagrant ssh -c "fleetctl status ircdd"
+else
+    echo "All clear"
+fi
