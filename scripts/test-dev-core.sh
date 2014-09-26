91805edee6b9b2aa21b9f2f46457bf50ba27df39
Kiril Zvezdarov <kzvezdarov@gmail.com>
2014-09-26 13:00:15 -0500

    Dockerfile can now be used to run the daemon

--- scripts/test-dev-core.sh
+++ scripts/test-dev-core.sh
@@ -2,4 +2,4 @@
 
 cd `dirname $0`/dev-config
 vagrant rsync
-vagrant ssh -c "docker build -t ircdd-test ircdd/scripts/dev-config && docker run -v /home/core/ircdd:/data/ircdd-git ircdd-test" 
+vagrant ssh -c "docker build -t ircdd-test ircdd/scripts/dev-config && docker run -v /home/core/ircdd:/data ircdd-test bash -c  \"python setup.py flake8 && python setup.py nosetests && python setup.py install\""
