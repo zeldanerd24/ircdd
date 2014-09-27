b51d375974ae8b377ba280da99a6b9a77f499ab1
Dan M McGinnis <mcginnis.dan@gmail.com>
2014-09-26 22:09:31 -0400

    second version of implementing issue #9

--- bin/ircdd.py
+++ bin/ircdd.py
@@ -11,11 +11,14 @@ if __name__ == "__main__":
 #  to only accept certain types of input. Be warned that this will cause an error that
 #  exits the program. 
 # To make it an optional flag proceed the name with a '-' or '--'
-    flag_parser.add_argument("-hn", "--hostname", help="hostname for the server")
-    flag_parser.add_argument("-p", "--portnumber", help="portnumber for the server",
+    flag_parser.add_argument("-hn", "--hostname", help="The Hostname for the server")
+    flag_parser.add_argument("-p", "--portnumber", help="The Portnumber for the server",
                          type=int)
  
     valid_args, unknown = flag_parser.parse_known_args()
     input_flags = vars(valid_args)
     flags = {x: input_flags[x] for x in input_flags if input_flags[x] is not None}
+
+    if unknown:
+        print "The following flags are invalid and have been ignored by IRCDD:  ", ', '.join(map(str, unknown))
 
