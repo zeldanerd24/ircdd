60ad531caa89b568edab6191ce1b633221466a1d
Michael Harrison <michael.harrison0002@temple.edu>
2014-10-14 17:56:28 -0400

    Implements: 	-ConfigStore: context object containing command line parameters and shared access modules 	-Order of precedence (cmd line > config file > defaults)
    Resolves #10

--- ircdd/context.py
+++ ircdd/context.py
@@ -5,6 +5,8 @@ from twisted.cred import portal
 from twisted.words import service
 import server
 
+import yaml
+
 userdata = dict(
     kzvezdarov='password',
     mcginnisdan='password',
@@ -14,6 +16,19 @@ userdata = dict(
     )
 
 
+class ConfigStore(dict):
+    """
+    Container for configuration values and shared acces modules.
+    """
+    data = {'host': 'localhost', 'port': '5799'}
+
+    def __getitem__(self, key):
+        return self.data[key]
+
+    def __setitem__(self, key, item):
+        self.data[key] = item
+
+
 def makeContext(config):
     """
     Constructs an initialized context from the config values.
@@ -21,12 +36,29 @@ def makeContext(config):
         A dict mapping keys to available resources,
         including the original config values.
     """
-    ctx = dict(config)
+
+    ctx = ConfigStore()
+
+    # if user specified a configuration file
+    # overwrite defaults with values from file
+    if config.get('config') is not None:
+        stream = file(config.get('config'), 'r')
+        del config['config']
+        # yaml.load turns a file into an object/dictionary
+        conFile = yaml.load(stream)
+        stream.close()
+        for x in conFile:
+            ctx[x] = conFile.get(x)
+
+    # if user specified any values via command line
+    # overwrite existing values
+    for x in config:
+        ctx[x] = config.get(x)
 
     # TODO: Initialize DB driver
-    # ctx.rethinkdb =
+    # ctx['rethinkdb'] =
     # TODO: Initialize NSQ driver
-    # ctx.nsq =
+    # ctx['nsq'] =
 
     # TODO: Make a custom realm that integrates with the database?
     ctx['realm'] = service.InMemoryWordsRealm('placeholder_realm')
