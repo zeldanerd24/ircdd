29735f07bb20c1ca220164982fcaddc4be36a229
Kiril Zvezdarov <kzvezdarov@gmail.com>
2014-10-22 19:53:37 -0400

    Cleaned up RemoteReadWriter and added docstrings

--- ircdd/remote.py
+++ ircdd/remote.py
@@ -2,36 +2,70 @@ import nsq
 
 
 class RemoteReadWriter:
-    _observers = {}
-    _writer = None
-    _readers = {}
-    _lookupd_addresses = None
-    _server_name = None
+    """
+    A high level producer/consumer for publishing/consuming from NSQ.
+    Maintains a single long-lived `nsq.Writer`, a set of `nsq.Reader`s,
+    and a set of callbacks. The mapping between `Reader`s and callbacks is 1:1.
+
+    Expects a list of `NSQD` and `NSQLookupd` addresses to be passed along with
+    a unique server identifier.
+
+    :param nsqd_addresses: a list of strings in the form 'tcp_address:port'
+    that correspond to ``NSQD`` instances that the writer should publish to.
+    :type list:
+
+    :param lookupd_addresses: a list of strings in the form 'http_address:port'
+    that correspond to ``NSQLookupd`` instances that the readers should poll
+    for producers.
+    :type list:
+
+    :param server_name: a string that uniquely idetifies this server instance.
+    It will be used as channel name for both publishing and reading from `NSQ`.
+    :type string:
+    """
 
     def __init__(self, nsqd_addresses, lookupd_addresses, server_name):
+        self._readers = {}
+
         self._writer = nsq.Writer(nsqd_addresses)
         self._lookupd_addresses = lookupd_addresses
         self._server_name = server_name
 
     def subscribe(self, topic, callback):
-        self._callbacks[topic] = callback
+        """
+        Used to subscribe a callback to a topic. Typically used by
+        :class:`ircdd.server.IRCDDUser` and :class:`ircdd.server.IRCDDGroup`.
+        It will spin up a new :class:`nsq.Reader` for the given topic if one
+        does not exist already and register the given callback with it.
+        Only one callback per topic exists.
+
+        :param topic: a string which identifies the topic on which to listen.
+        :type string:
+
+        :param callback: the callback which will be called when a message is
+        read. The callback must take a single argument (`message`) and either
+        call :meth:`nsq.Message.finish()`/:meth:`nsq.Message.requeue()` or
+        return `True`/`False` when it is done with the message.
+        :type callable:
+        """
 
         if self._readers.get(topic, None) is None:
-            reader = nsq.Reader(message_handler=self.filtered_callback(topic),
-                                lookupd_http_addresses=self.lookupd_addresses,
+            reader = nsq.Reader(message_handler=callback,
+                                lookupd_http_addresses=self._lookupd_addresses,
                                 topic=topic,
-                                channel=self.server_name,
+                                channel=self._server_name,
                                 lookupd_poll_interval=15)
             self._readers[topic] = reader
             nsq.run()
 
-    def unsibscribe(self, topic):
-        self._callbacks[topic] = None
-        self._readers[topic].close()
+    def unsubscribe(self, topic):
+        """
+        Unsubscribes a callback from the given topic. Typically used by
+        :class:`ircdd.server.IRCDDUser` and :class:`ircdd.server.IRCDDGroup`.
+        It will shut down the reader.
 
-    def filter_callback(self, topic):
-        def filtered_callback(message):
-            # discard messages which originate here
-            self._observers[topic](message)
-            return True
-    return filtered_callback
+        :param topic: the topic for which to stop listening.
+        :type string:
+        """
+        self._readers[topic].close()
+        del self._readers[topic]
