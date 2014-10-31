d0710dacf7211c74d50a5a8c35d9ec0f0496003d
Michael Harrison <michael.harrison0002@temple.edu>
2014-10-31 14:27:35 -0400

    Added commands to launch NSQ, NSQAdmin, and NSQLookup.

--- scripts/run-dev-server.sh
+++ scripts/run-dev-server.sh
@@ -5,9 +5,28 @@ vagrant rsync
 vagrant ssh -c "fleetctl stop ircdd.service"
 vagrant ssh -c "fleetctl destroy ircdd.service"
 vagrant ssh -c "fleetctl start ircdd/scripts/dev-config/services/ircdd.service"
+# Enable database
 vagrant ssh -c "fleetctl stop rethinkdb@.service"
 vagrant ssh -c "fleetctl destroy rethinkdb@.service"
 vagrant ssh -c "fleetctl stop rethinkdb@ircdd.service"
 vagrant ssh -c "fleetctl destroy rethinkdb@ircdd.service"
 vagrant ssh -c "fleetctl submit ircdd/scripts/dev-config/services/rethinkdb@.service"
 vagrant ssh -c "fleetctl start ircdd/scripts/dev-config/service/rethinkdb@ircdd.service"
+# Enable message queue
+vagrant ssh -c "fleetctl stop nsqd.service"
+vagrant ssh -c "fleetctl destroy nsqd.service"
+vagrant ssh -c "fleetctl start ircdd/scripts/dev-config/services/nsqd.service"
+# Enable message queue admin
+vagrant ssh -c "fleetctl stop nsqadmin@.service"
+vagrant ssh -c "fleetctl destroy nsqadmin@.service"
+vagrant ssh -c "fleetctl stop nsqadmin@first.service"
+vagrant ssh -c "fleetctl destroy nsqadmin@first.service"
+vagrant ssh -c "fleetctl submit ircdd/scripts/dev-config/services/nsqadmin@.service"
+vagrant ssh -c "fleetctl start ircdd/scripts/dev-config/service/nsqadmin@first.service"
+# Enable message queue search
+vagrant ssh -c "fleetctl stop nsqlookupd@.service"
+vagrant ssh -c "fleetctl destroy nsqlookupd@.service"
+vagrant ssh -c "fleetctl stop nsqlookupd@search.service"
+vagrant ssh -c "fleetctl destroy nsqlookupd@search.service"
+vagrant ssh -c "fleetctl submit ircdd/scripts/dev-config/services/nsqlookupd@.service"
+vagrant ssh -c "fleetctl start ircdd/scripts/dev-config/service/nsqlookupd@search.service"
