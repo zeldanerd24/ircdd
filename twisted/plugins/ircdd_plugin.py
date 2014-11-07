2a16097412955c063fa6e435f20170cd3d49b9e1
Kiril Zvezdarov <kzvezdarov@gmail.com>
2014-11-07 00:29:58 -0500

    Added DB name to CLI args

--- twisted/plugins/ircdd_plugin.py
+++ twisted/plugins/ircdd_plugin.py
@@ -21,8 +21,9 @@ class Options(usage.Options, strcred.AuthOptionMixin):
     supportedInterfaces = (credentials.IUsernamePassword)
 
     optParameters = [
-        ["hostname", "h", "127.0.0.1"],
-        ["port", "p", 5799],
+        ["hostname", "H", "127.0.0.1"],
+        ["port", "P", 5799],
+        ["db", "D", "ircdd"],
         ["rdb_port", "", 28015],
         ["rdb_hostname", "", "127.0.0.1"],
         ]
