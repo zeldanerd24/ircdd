56e73891b768a560beb59215ba901c6dc2dd218a
Kiril Zvezdarov <kzvezdarov@gmail.com>
2014-11-08 22:27:07 -0500

    Added RDB and NSQLookupd port exposure
    Fixed timeouts and added environment
    
    Modified NSQL service to support discovery sidekick:

--- scripts/dev-config/vm-config.rb
+++ scripts/dev-config/vm-config.rb
@@ -46,6 +46,8 @@ $enable_serial_logging=false
 #   export DOCKER_HOST='tcp://127.0.0.1:2375'
 #$expose_docker_tcp=3375
 $expose_app_tcp=2375
+$expose_rdb_tcp=28015
+$expose_nsqlookupd_http=4161
 
 # Setting for VirtualBox VMs
 $vb_gui = false
